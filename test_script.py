62a11f462dae47e7e1c145098b176e199a43d504
tandavala <tandavala@github.com>
2018-01-02 15:27:58 +0100

    adding some tests

--- test_script.py
+++ test_script.py
@@ -1,1 +1,13 @@
 # This is the script for our test
+
+import unittest
+
+class ForVideo(unittest.TestCase):
+    def setUp(self):
+        print("some text")
+    def tearDown(self):
+        print("some text")
+    
+if __name__=="__main__":
+    unittest.main()
+
